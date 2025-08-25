b504b543508af6da1c9df4cc4da27011cf6f62bd
cgew92 <james.che27@bcp.org>
2025-08-24 17:51:56 -0700

    update

--- script.js
+++ script.js
@@ -192,7 +192,7 @@ function createPaperCard(paper) {
             </div>
             <div class="pub-actions">
                 <a href="${paper.pdf_url}" class="pub-download" target="_blank">
-                    <i data-feather="download"></i> Download PDF
+                    <i id="download" data-feather="download"></i> Download PDF
                 </a>
             </div>
         </div>
